0d752cf5e4048bde7f4f7bac2764d4329f0cd2fd
PatMurf <murphypatv@gmail.com>
2014-04-02 10:23:03 +0100

    fix bug where admin could not add new user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :type, :email, :password, :superadmin
+  permit_params :email, :password, :password_confirmation, :superadmin
 
   index do
     column :email
@@ -16,26 +16,9 @@ ActiveAdmin.register User do
       f.input :email
       f.input :password
       f.input :password_confirmation
-      f.input :superadmin, :label => "Super Administrator"
+      f.input :superadmin
     end
     f.actions
   end
-
-  # code source= http://dan.doezema.com/2012/02/how-to-implement-a-single-user-model-with-rails-activeadmin-and-devise/
-  create_or_edit = Proc.new {
-    @user            = User.find_or_create_by_id(params[:id])
-    @user.superadmin = params[:user][:superadmin]
-    @user.attributes = params[:user].delete_if do |k, v|
-      (k == "superadmin") ||
-      (["password", "password_confirmation"].include?(k) && v.empty? && !@user.new_record?)
-    end
-    if @user.save
-      redirect_to :action => :show, :id => @user.id
-    else
-      render active_admin_template((@user.new_record? ? 'new' : 'edit') + '.html.erb')
-    end
-  }
-  member_action :create, :method => :post, &create_or_edit
-  member_action :update, :method => :put, &create_or_edit
 
 end
