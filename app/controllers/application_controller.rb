65990ca189c6c1fed1ff3204957e894c27c8b449
PatMurf <murphypatv@gmail.com>
2014-05-02 12:58:49 +0100

    redirect to project list when logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # redirect to project list page after signing in
+  def after_sign_in_path_for(resource)
+    projects_path
+  end
+
   # only allow admin user to access admin area
 	def authenticate_active_admin_user!
     authenticate_user!
@@ -12,3 +17,5 @@ class ApplicationController < ActionController::Base
     end
   end
 end
+
+
